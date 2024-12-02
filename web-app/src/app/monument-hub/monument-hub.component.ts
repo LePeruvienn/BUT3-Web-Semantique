86f3b310abce70482dfc8b53004d426dd317f2a2
Miasmax <2016maxe@gmail.com>
2024-12-02 20:03:28 +0100

    first working app

--- web-app/src/app/monument-hub/monument-hub.component.ts
+++ web-app/src/app/monument-hub/monument-hub.component.ts
@@ -20,8 +20,22 @@ import { query } from '@angular/animations';
 
 export class MonumentHubComponent{
   query: string = '';
+  _results: any[] = [];
+  _parseData: { Country: string; Value: string }[] = []; 
+  listCountryData = [
+    {Country: "", Value: 0},
+  ];
+  showGraph: boolean = false; // Controls the visibility of the section
+
+  private metric: string = "absolute"; // "absolute" or "percentage"
+  private width: number = 800; // Chart width
+
 
   constructor(private sharedService: SharedService) {}
+
+  get data(): string {
+    return this.sharedService.getData();
+  }
   
   getText(): void {
     const textarea = document.getElementById('myTextarea') as HTMLTextAreaElement;
@@ -30,45 +44,39 @@ export class MonumentHubComponent{
     }
   }
 
-  get data(): string {
-    return this.sharedService.getData();
+  LoadData() {
+    const n3Data = this.data
+    this.getText();
+    this.queryRequestToData(n3Data,this.query)
   }
-  
-  listCountry: string[] = ['USA', 'France', 'Germany', 'Canada']; // List of countries for the choice
-  selectedCountry: string = ''; // Tracks the selected country
-  showGraph: boolean = false; // Controls the visibility of the section
 
-  private metric: string = "absolute"; // "absolute" or "percentage"
-  private width: number = 800; // Chart width
-  private states = [ //DATA
-    { State: "Alabama", 2010: 100, 2019: 120 },
-    { State: "Alaska", 2010: 80, 2019: 60 },
-    { State: "Arizona", 2010: 90, 2019: 100 },
-    { State: "California", 2010: 102, 2019: 95 },
-    { State: "Texas", 2010: 78, 2019: 120 },
-    { State: "New York", 2010: 89, 2019: 93 },
-    { State: "Florida", 2010: 95, 2019: 110 },
-    { State: "Georgia", 2010: 72, 2019: 85 },
-    { State: "Illinois", 2010: 105, 2019: 112 }
-    // Add more data as needed
-  ];
+  showResults(): void {
+    console.log ("SHOW RESULTS")
+    this.parseDataForGraph()
+    this.createGraph();
+  }
 
-
-  async onCountryChange() {
-    this.showGraph = this.selectedCountry !== ''; // Show section if a country is selected
-    if(this.showGraph){
-      const n3Data = this.data
-      this.getText();
-      const results = this.queryRequestToData(n3Data,this.query)
-
-      if (results){
-        console.log("results")
-        console.log(results["1"])
-        console.log(results[1]["?disaster"])
-        console.log("results")
+  parseDataForGraph(){
+    this._results.forEach(result => {
+      const countryName = result["?countryName"]?.value;
+      
+      // Vérifie si le pays existe déjà dans listCountryData
+      const country = this.listCountryData.find(item => item.Country === countryName);
+      
+      if (country) {
+        // Si le pays existe, on incrémente sa valeur
+        country.Value += 1;
+      } else if (countryName) {
+        // Si le pays n'existe pas, on l'ajoute avec une valeur par défaut (par exemple, 1)
+        this.listCountryData.push({ Country: countryName, Value: 1 });
       }
-      //this.createGraph();
-    }
+    });
+    // Tri de la liste par valeur décroissante et garde seulement les 20 premiers pays
+    this.listCountryData = this.listCountryData
+    .sort((a, b) => b.Value - a.Value)
+    .slice(0, 10);  // Garde les 20 premiers éléments
+    console.log(this.listCountryData)
+
   }
 
   queryRequestToData(n3Data: string,query: string) {
@@ -90,31 +98,17 @@ export class MonumentHubComponent{
       console.error('Invalid SPARQL query.');
       return;
     }
-
+    
+    const results: any[] = [];
     store.query(sparqlQuery, function(result) {
       console.log('query ran');
-      console.log(result);
-  });
+      results.push(result);
+     });
 
-
-    return [];
+    this._results = results;
   }
-
-
   
   getQuery(): string {
-    const query1 = `
-    @base <https://cours.iut-orsay.fr/qar/> .
-    PREFIX iut: <https://cours.iut-orsay.fr/qar/>
-    PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
-    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-
-    select ?disaster where {
-
-    ?disaster rdf:type iut:NaturalDisaster.
-    
-    }
-    `;
     const query = `
     @base <https://cours.iut-orsay.fr/qar/> .
     PREFIX iut: <https://cours.iut-orsay.fr/qar/>
@@ -132,84 +126,69 @@ export class MonumentHubComponent{
     }
     GROUP BY ?country ?countryName
     ORDER BY DESC (?nbPays)
+    LIMIT 10
     `;
     return query;
   }
 
- 
   createGraph(): void { //D3 GRAPH
-    const data = d3.sort(this.states, d => d[2019] - d[2010])
-    .map(d => ({
-      ...d,
-      value: this.metric === "absolute" 
-        ? d[2019] - d[2010] 
-        : (d[2019] - d[2010]) / d[2010]
+
+    const data = this.listCountryData.map(d => ({
+      Country: d.Country,
+      Value: d.Value,
     }));
 
-    const barHeight = 25;
-    const marginTop = 30;
-    const marginRight = 60;
-    const marginBottom = 10;
-    const marginLeft = 60;
-    const height = Math.ceil((data.length + 0.1) * barHeight) + marginTop + marginBottom;
+  // Specify the chart’s dimensions.
+  const width = 928;
+  const height = 500;
+  const marginTop = 20;
+  const marginRight = 0;
+  const marginBottom = 30;
+  const marginLeft = 40;
 
-    const x = d3.scaleLinear()
-      .domain(d3.extent(data, d => d.value) as [number, number])
-      .rangeRound([marginLeft, this.width - marginRight]);
+  // Create the horizontal scale and its axis generator.
+  const x = d3.scaleBand()
+    .domain(data.map(d => d.Country)) // Use Country for the x axis
+    .range([marginLeft, width - marginRight])
+    .padding(0.1);
 
-    const y = d3.scaleBand()
-      .domain(data.map(d => d.State))
-      .rangeRound([marginTop, height - marginBottom])
-      .padding(0.1);
+  const xAxis = d3.axisBottom(x).tickSizeOuter(0);
 
-    const format = d3.format(this.metric === "absolute" ? "+,d" : "+.1%");
-    const tickFormat = this.metric === "absolute" 
-      ? d3.formatPrefix("+.1", 1e6) 
-      : d3.format("+.0%");
+  // Create the vertical scale.
+  const y = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.Value) ?? 0])
+    .range([height - marginBottom, marginTop]);
 
-    // Create an SVG element using d3.create()
-    const svg = d3.create("svg")
-      .attr("viewBox", `0 0 ${this.width} ${height}`)
-      .attr("style", "max-width: 100%; height: auto; font: 10px sans-serif;");
+  // Create the SVG container.
+  const svg = d3.create("svg")
+    .attr("viewBox", [0, 0, width, height])
+    .attr("width", width)
+    .attr("height", height)
+    .attr("style", "max-width: 100%; height: auto;");
 
-      console.log(svg)
+  // Append the bars.
+  svg.append("g")
+    .attr("class", "bars")
+    .attr("fill", "steelblue")
+  .selectAll("rect")
+  .data(data)
+  .join("rect")
+    .attr("x", d => x(d.Country) ?? 0) // Use Country for x-axis positioning
+    .attr("y", d => y(d.Value) ?? 0) // Use Value for the y-axis
+    .attr("height", d => Math.max(0, y(0) - y(d.Value))) // Ensure height is never undefined or negative
+    .attr("width", x.bandwidth());
 
-    svg.append("g")
-      .selectAll("rect")
-      .data(data)
-      .join("rect")
-      .attr("fill", d => d3.schemeRdBu[3][d.value > 0 ? 2 : 0])
-      .attr("x", d => x(Math.min(d.value, 0)))
-      .attr("y", d => y(d.State)!)
-      .attr("width", d => Math.abs(x(d.value) - x(0)))
-      .attr("height", y.bandwidth());
+  // Append the axes.
+  svg.append("g")
+    .attr("class", "x-axis")
+    .attr("transform", `translate(0,${height - marginBottom})`)
+    .call(xAxis);
 
-    svg.append("g")
-      .selectAll("text")
-      .data(data)
-      .join("text")
-      .attr("text-anchor", d => (d.value < 0 ? "end" : "start"))
-      .attr("x", d => x(d.value) + Math.sign(d.value - 0) * 4)
-      .attr("y", d => y(d.State)! + y.bandwidth() / 2)
-      .attr("dy", "0.35em")
-      .text(d => format(d.value));
-
-    svg.append("g")
-      .attr("transform", `translate(0,${marginTop})`)
-      .call(d3.axisTop(x).ticks(this.width / 80).tickFormat(tickFormat))
-      .call(g => g.selectAll(".tick line").clone()
-        .attr("y2", height - marginTop - marginBottom)
-        .attr("stroke-opacity", 0.1))
-      .call(g => g.select(".domain").remove());
-
-    svg.append("g")
-      .attr("transform", `translate(${x(0)},0)`)
-      .call(d3.axisLeft(y).tickSize(0).tickPadding(6))
-      .call(g => g.selectAll(".tick text")
-        .filter((d, i) => data[i].value < 0)
-        .attr("text-anchor", "start")
-        .attr("x", 6));
-    //D3 GRAPH FIN
+  svg.append("g")
+    .attr("class", "y-axis")
+    .attr("transform", `translate(${marginLeft},0)`)
+    .call(d3.axisLeft(y))
+    .call(g => g.select(".domain").remove());
 
 
     // Select the container (with id="chart") and ensure it's available
